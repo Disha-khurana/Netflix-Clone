c8ef729347428b16a0ab7b27668877eb003cc686
Disha <131692855+Disha-khurana@users.noreply.github.com>
2024-08-17 15:28:56 +0530

    Update axios.js

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: `https://api.themoviedb.org/3/`,
-    // headers: {
-    //     accept: 'application/json',
-    //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZDg2NWU0MjdiZmRmOWQ4YmY1OTVlYmI0Y2FmZWFkYiIsIm5iZiI6MTcyMTcxMjQzNi42MzM5OTIsInN1YiI6IjY2OWRlOGYyM2YzY2MzM2I5ODQ3YTJhNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-m2ELD6LF3Hn7-dGncBCe86fWdzz4dsk_GNu_t6i6vs'
-    //   }
 
 })
 
